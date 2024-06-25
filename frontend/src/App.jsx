9460c0d78cab5f4c6f397845bb9750a523edf63a
TaslimCooasis <shamim.ahmad159@gmail.com>
2024-06-25 15:59:03 +0530

    changes done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
 import Slider from "react-slick";
 import "./App.css";
-import EcosystemSignup from "./pages/EcosystemSignup/EcosystemSignup";
-import EmailVerify from "./pages/emailVerify/EmailVerify";
+import EcosystemSignup from "./components/EcosystemSignup/EcosystemSignup";
+import EmailVerify from "./components/emailVerify/EmailVerify";
 import { Routes, Route } from "react-router-dom";
-import ReferEarn from "./components/mobileResponsive/referEarn/ReferEarn";
-import StYourJourney from "./components/startYourJourney/StYourJourney";
+// import ReferEarn from "./components/mobileResponsive/referEarn/ReferEarn";
+// import StYourJourney from "./components/startYourJourney/StYourJourney";
 
 function App() {
   return (
     <div className="">
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<EcosystemSignup />} />
         <Route path="/verify" element={<EmailVerify />} />
-      </Routes> */}
-      <ReferEarn />
-      <StYourJourney />
+      </Routes>
+      {/* <ReferEarn />
+      <StYourJourney /> */}
     </div>
   );
 }
