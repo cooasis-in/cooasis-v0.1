1654bc4897e3c8543937875d2ca8c246d4e5d461
Shahbaz-cooasis <shahbaz@cooasis.in>
2024-06-26 20:03:15 +0530

    newcompadded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,21 @@ import { Routes, Route } from "react-router-dom";
 import ReferEarn from "./components/mobileResponsive/referEarn/ReferEarn";
 import VerifiedResp from "./components/mobileResponsive/verified/VerifiedResp";
 import SliderMob from "./components/mobileResponsive/sliderResp/SliderMob";
+import PenTool from "./components/penTool/PenTool";
+import PenToolUi from "./components/penTool/PenToolUi";
 
 function App() {
   return (
     <div className="">
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<EcosystemSignup />} />
         <Route path="/verify" element={<ReferEarn />} />
         <Route path="/verified" element={<VerifiedResp />} />
         <Route path="/refer" element={<ReferEarn />} />
         <Route path="/slider" element={<SliderMob />} />
-      </Routes>
+      </Routes> */}
+      {/* <PenTool/> */}
+      <PenToolUi/>
     </div>
   );
 }
