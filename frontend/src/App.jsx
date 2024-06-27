18087dbbd02faf1145c29fe1fd6a7006bc4e1f64
TaslimCooasis <shamim.ahmad159@gmail.com>
2024-06-27 20:38:12 +0530

    Fixed conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,26 @@
-// import Slider from "react-slick";
+import Slider from "react-slick";
 import "./App.css";
 
 import FillInYourDetails from "./components/FillInYourDetails/FillInYourDetails";
-// import EcosystemSignup from "./pages/EcosystemSignup/EcosystemSignup";
-// import { Routes, Route } from "react-router-dom";
-// import ReferEarn from "./components/mobileResponsive/referEarn/ReferEarn";
-// import VerifiedResp from "./components/mobileResponsive/verified/VerifiedResp";
-// import SliderMob from "./components/mobileResponsive/sliderResp/SliderMob";
 import EcosystemSignup from "./pages/EcosystemSignup/EcosystemSignup";
 import { Routes, Route } from "react-router-dom";
 import ReferEarn from "./components/mobileResponsive/referEarn/ReferEarn";
 import VerifiedResp from "./components/mobileResponsive/verified/VerifiedResp";
 import SliderMob from "./components/mobileResponsive/sliderResp/SliderMob";
-import PenTool from "./components/penTool/PenTool";
+// import PenTool from "./components/penTool/PenTool";
 import PenToolUi from "./components/penTool/PenToolUi";
 
 function App() {
   return (
     <div className="">
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<EcosystemSignup />} />
         <Route path="/verify" element={<ReferEarn />} />
         <Route path="/verified" element={<VerifiedResp />} />
         <Route path="/refer" element={<ReferEarn />} />
         <Route path="/slider" element={<SliderMob />} />
-      </Routes> */}
-      <FillInYourDetails/>
+        <Route path="/fill-your-details" element={<FillInYourDetails />} />
+      </Routes>
       {/* <PenTool/> */}
       <PenToolUi/>
     </div>
